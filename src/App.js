16b110b50cceb983953b897314706d53c71c28fd
Pritish Ghosh <pritish.ghosh@grazitti.local>
2024-01-16 23:09:30 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
-import Main from './components/Main';
-import Navbar from './components/Navbar';
+import Home from './components/Home';
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom'
+import Test from './components/Test';
+
 
 function App() {
   return (
     <div className='app-container' id='running-app'>
-      <Navbar />
-      <Main />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Test/>} />
+          <Route path="/about" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Test.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Test = () => {
+  return (
+    <div>
+      testing
+    </div>
+  )
+}
+
+export default Test
